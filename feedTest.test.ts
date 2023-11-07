d71e20fdfe3080b3e761b9fa6069c7611a360131
BayknBoi <149636411+BayknBoi@users.noreply.github.com>
2023-11-06 20:33:43 -0700

    Update feedTest.test.ts

--- feedTest.test.ts
+++ feedTest.test.ts
@@ -25,8 +25,36 @@ const hippoFood = ["grass", "reeds", "shoots"];
 const badFood = ["shrimp", "potatoes", "pizza", "icecream"];
 
 describe("feeding animals", () => {
-  test("Alex likes the food we expect", () => {});
-  test("Marty likes the food we expect", () => {});
-  test("Melman likes the food we expect", () => {});
-  test("Gloria likes the food we expect", () => {});
+  test("Alex likes the food we expect", () => {
+      lionFood.forEach((food) =>{
+          expect(alex.feed(food)).toBe("Alex the Lion likes ${food}!")
+      })
+  });
+    badFood.forEach((food) =>{
+        exper(alex.feed(food)).toBo("Alex the Lion doesn't like ${food}!")
+    })
+  test("Marty likes the food we expect", () => {
+      zebraFood.forEach((food) =>{
+          expect(marty.feed(food)).toBe("Marty the Zebra Likes ${food}!")
+      })
+  });
+    badFood.forEach((food)=> {
+        expect(marty.feed(food)).toBe("Marty the Zebra doesn't likes ${food}!")
+    })
+  test("Melman likes the food we expect", () => {
+    giraffeFood.forEach((food) => {
+      expect(melman.feed(food)).toBe(`Melman the Giraffe likes ${food}!`)
+    })
+  });
+  badFood.forEach((food) => {
+    expect(melman.feed(food)).toBe(`Melman the Giraffe doesn't like ${food}!`)
+  })
+  test("Gloria likes the food we expect", () => {
+    hippoFood.forEach((food) => {
+      expect(gloria.feed(food)).toBe(`Gloria the Hippo likes ${food}!`)
+    })
+  });
+  badFood.forEach((food) => {
+    expect(gloria.feed(food)).toBe(`Gloria the Hippo doesn't like ${food}!`)
+  })
 });
